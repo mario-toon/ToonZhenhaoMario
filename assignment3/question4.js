d7c411b4feaa856085261e1121ba6ec353106e11
mario-toon <mario.toon@gmail.com>
2025-07-15 23:17:27 +0800

    Completed assignment3

--- assignment3/question4.js
+++ assignment3/question4.js
@@ -1,8 +1,10 @@
 const disneyData = {
-	open: "120.54",
-	high: "122.56",
-	low: "120.54",
-	close: "121.09",
+  open: "120.54",
+  high: "122.56",
+  low: "120.54",
+  close: "121.09",
 };
 
 // Write your code below
+disneyData.volume = "1512311";
+console.log(disneyData);
--- assignment3/question5.js
+++ assignment3/question5.js
@@ -1,3 +1,25 @@
 const bacData = [190.15, 194.21, 191.07, 192.44, 129000];
 
 // Write your code below
+const bacDataLow = [bacData[0]];
+const bacDataHigh = [bacData[1]];
+const bacDataOpen = [bacData[2]];
+const bacDataClose = [bacData[3]];
+const bacDataVol = [bacData[4]];
+
+if (bacDataClose > bacDataOpen) {
+  if (bacDataVol > 100000) {
+    console.log("Strong Bullish");
+  } else {
+    console.log("Bullish");
+  }
+} else if (bacDataClose < bacDataOpen) {
+  if (bacDataVol > 10000) {
+    console.log("Strong Bearish");
+  } else {
+    console.log("Bearish");
+  }
+} else if (bacDataClose === bacDataOpen) {
+  console.log("Neutral");
+}
+// Expect Output = Strong Bullish
